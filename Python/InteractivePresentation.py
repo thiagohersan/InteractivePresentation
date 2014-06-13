b71898a2fc6a267552ea93037b99b11113150546
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-06-12 21:44:45 -0700

    simplifications for air presentation

--- Python/InteractivePresentation.py
+++ Python/InteractivePresentation.py
@@ -6,21 +6,21 @@ from calendar import timegm
 from threading import Thread
 from Queue import Queue
 from cPickle import dump, load
-from OSC import OSCClient, OSCMessage, OSCClientError, OSCServer, getUrlStr
-from nltk import pos_tag, word_tokenize
+from OSC import OSCClient, OSCMessage, OSCClientError
 from twython import TwythonStreamer
 from twilio.rest import TwilioRestClient
 
 ## What to search for
-SEARCH_TERMS = ["@nottoopublic", "#nottoopublic"]
+SEARCH_TERMS = ["@thiagohersan", "@radamar","#thiagohersan", "#radamar"]
 PHONE_NUMBER = "+15105647823"
+DISPLAY_ADDR = '127.0.0.1'
+DISPLAY_PORT = 8888
 
 class TwitterStreamReceiver(TwythonStreamer):
     def __init__(self, *args, **kwargs):
         super(TwitterStreamReceiver, self).__init__(*args, **kwargs)
         self.tweetQ = Queue()
     def on_success(self, data):
-        ## no re-tweets
         if ('text' in data):
             self.tweetQ.put(data['text'].encode('utf-8'))
             print "received %s" % (data['text'].encode('utf-8'))
@@ -31,19 +31,12 @@ class TwitterStreamReceiver(TwythonStreamer):
     def get(self):
         return self.tweetQ.get()
 
-def oscSubscribeHandler(addr, tags, args, source):
-    ip = getUrlStr(source).split(":")[0]
-    port = int(args[0])
-    print "subscribing %s:%s" % (ip,port)
-    myOscSubscribers[(ip,port)] = (ip,port)
-
 def setup():
     global lastTwitterCheck, myTwitterStream, streamThread
     global lastSmsCheck, mySmsClient, newestSmsSeconds
-    global myOscSubscribers, myOscServer, oscThread, myOscClient
+    global myOscClient
     global logFile
     secrets = {}
-    myOscSubscribers = {}
     lastTwitterCheck = time()
     lastSmsCheck = time()
     newestSmsSeconds = timegm(gmtime())
@@ -67,11 +60,6 @@ def setup():
                                    token=secrets['AUTH_TOKEN'])
 
     myOscClient = OSCClient()
-    myOscServer = OSCServer(('127.0.0.1', 8888))
-    myOscServer.addMsgHandler('/NotTooPublic/call', oscSubscribeHandler)
-    myOscServer.addMsgHandler('default', lambda addr, tags, args, source:None)
-    oscThread = Thread(target=myOscServer.serve_forever)
-    oscThread.start()
 
     ## open new file for writing log
     logFile = open("data/"+strftime("%Y%m%d-%H%M%S", localtime())+".log", "a")
@@ -79,22 +67,8 @@ def setup():
 def cleanTagAndSendText(text):
     ## removes punctuation
     text = re.sub(r'[.,;:!?*/+=\-&%^/\\_$~()<>{}\[\]]', ' ', text)
-    ## removes some bad words
-    text = re.sub(r'(f *u *c *k)', 'tuck', text)
-    text = re.sub(r'(s *h *i *t)', 'isht', text)
-    text = re.sub(r'(c *o *c *k)', 'dock', text)
-    text = re.sub(r'(d *i *c *k)', 'wick', text)
-    text = re.sub(r'(c *u *n *t)', 'grunt', text)
-    text = re.sub(r'(p *u *s *s *y)', 'juicy', text)
-    text = re.sub(r'(b *i *t *c *h)', 'itch', text)
-    text = re.sub(r'(a *s *s)', 'grass', text)
     ## replaces double-spaces with single space
     text = re.sub(r'( +)', ' ', text)
-
-    taggedText = pos_tag(text.split())
-    for (word,tag) in taggedText:
-        print "(%s:%s)" % (word,tag),
-    print " "
 
     ## log
     logFile.write(strftime("%Y%m%d-%H%M%S", localtime())+"***"+text+"\n")
@@ -102,26 +76,18 @@ def cleanTagAndSendText(text):
 
     ## forward to all subscribers
     msg = OSCMessage()
-    msg.setAddress("/NotTooPublic/response")
-    msg.append(" ".join([str(i[0]) for i in taggedText]))
-    msg.append(" ".join([str(i[1]) for i in taggedText]))
+    msg.setAddress("/airmsg/response")
+    msg.append(text)
 
-    delQ = Queue()
-    for (ip,port) in myOscSubscribers:
-        try:
-            myOscClient.connect((ip, port))
-            myOscClient.sendto(msg, (ip, port))
-            myOscClient.connect((ip, port))
-        except OSCClientError:
-            print "no connection to %s : %s, can't send message" % (ip, port)
-            delQ.put((ip,port))
-
-    while not delQ.empty():
-        del myOscSubscribers[delQ.get()]
-
+    try:
+        myOscClient.connect((DISPLAY_ADDR, DISPLAY_PORT))
+        myOscClient.sendto(msg, (DISPLAY_ADDR, DISPLAY_PORT))
+        myOscClient.connect((DISPLAY_ADDR, DISPLAY_PORT))
+    except OSCClientError:
+        print "no connection to %s : %s, can't send message" % (DISPLAY_ADDR, DISPLAY_PORT)
 
 def loop():
-    global lastTwitterCheck, myTwitterStream, streamThread, myOscSubscribers
+    global lastTwitterCheck, myTwitterStream, streamThread
     global lastSmsCheck, mySmsClient, newestSmsSeconds
     ## check twitter queue
     if((time()-lastTwitterCheck > 5) and (not myTwitterStream.empty())):
@@ -162,7 +128,5 @@ if __name__=="__main__":
     except KeyboardInterrupt :
         logFile.close()
         myTwitterStream.disconnect()
-        myOscServer.close()
         streamThread.join()
-        oscThread.join()
         sys.exit(0)
